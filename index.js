b3ec3aa987bf911973095e6bd8ef991855960334
gooking <javazj@qq.com>
2024-07-02 15:55:12 +0800

    userLogedList

--- index.js
+++ index.js
@@ -1195,6 +1195,9 @@ module.exports = {
   loginout: (token) => {
     return request('/user/loginout', true, 'get', { token })
   },
+  userLogedList: (token) => {
+    return request('/user/logedUserList', true, 'get', { token })
+  },
   userDelete: (token) => {
     return request('/user/delete', true, 'post', { token })
   },
