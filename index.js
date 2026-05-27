4ab771363f214a440bf824b079946aed1092538e
gooking <javazj@qq.com>
2026-05-27 19:31:03 +0800

    '26.5.27'

--- index.js
+++ index.js
@@ -1084,7 +1084,7 @@ module.exports = {
     return request('/dfs/upload/list/v2', true, 'post', data)
   },
   galleryList: data => {
-    return request('/dfs/gallery', true, 'post', data)
+    return request(COMMON_BASE_URL + subDomain + '/dfs/gallery', false, 'post', data)
   },
   refundApply: (data) => {
     return request('/order/refundApply/apply', true, 'post', data)
@@ -1934,6 +1934,9 @@ module.exports = {
   },
   partnerBuyTeamLeader: token => {
     return request('/partner/buy', true, 'post', { token })
+  },
+  partnerBuyyPartner: token => {
+    return request('/partner/buyPartner', true, 'post', { token })
   },
   partnerMembersStatistics: token => {
     return request(COMMON_BASE_URL + subDomain + '/partner/members/statistics', false, 'get', { token })
@@ -3052,5 +3055,14 @@ module.exports = {
   },
   parentStudentMyParents: (data) => {
     return request(COMMON_BASE_URL + subDomain + '/parentStudent/myParents', false, 'post', data)
+  },
+  gameNumberBombBegin: (data) => {
+    return request(COMMON_BASE_URL + subDomain + '/gameNumberBomb/begin', false, 'post', data)
+  },
+  gameNumberBombGuess: (data) => {
+    return request(COMMON_BASE_URL + subDomain + '/gameNumberBomb/guess', false, 'post', data)
+  },
+  gameNumberBombLogs: (data) => {
+    return request(COMMON_BASE_URL + subDomain + '/gameNumberBomb/logs', false, 'post', data)
   },
 }
